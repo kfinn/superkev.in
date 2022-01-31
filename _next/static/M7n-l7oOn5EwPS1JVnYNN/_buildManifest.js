a9af0e260770e53375a436b839aa20d652d8719c
Kevin Finn <superkevin@gmail.com>
2022-01-31 18:09:09 -0500

    Updates

---
+++ _next/static/M7n-l7oOn5EwPS1JVnYNN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1ed11d855cb94c05.css","static/chunks/pages/index-8ef10285031da1bf.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
