14966c5e6521b2aa793abc451dfc17e31a54f656
Kevin Finn <superkevin@gmail.com>
2022-01-31 22:59:49 -0500

    add more favicon business

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const debug = process.env.NODE_ENV !== 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: debug ? "" : "/superkev.in"
 }
 
 module.exports = nextConfig
