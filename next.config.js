7014e134b3dead8d9330b1408b6b1b5499094665
Kevin Finn <superkevin@gmail.com>
2022-02-03 19:24:06 -0500

    remove base path to support hosting from superkev.in domain

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const debug = process.env.NODE_ENV !== 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: debug ? "" : "/superkev.in"
 }
 
 module.exports = nextConfig
